a04c3fb30f297ff2627ccde931847f8b0700ee10
dinitha2004 <158298460+dinitha2004@users.noreply.github.com>
2024-05-05 06:01:15 -0700

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </div>
         <hr />
         <div className="footer-bottom">
-            <p className="footer-bottom-left">2023 Chalani Dintha.All right reserved.</p>
+            <p className="footer-bottom-left">2024 Chalani Dintha.All right reserved.</p>
             <div className="footer-bottom-right">
                 <p>Term of Services</p>
                 <p>Privacy Policy</p>
